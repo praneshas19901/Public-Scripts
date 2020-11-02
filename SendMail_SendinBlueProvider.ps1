32031bc0c2433fa833c82e6138879c17398783bb
praneshshzl <69966286+praneshshzl@users.noreply.github.com>
2020-11-02 20:47:04 +0530

    Add files via upload

---
+++ SendMail_SendinBlueProvider.ps1
@@ -0,0 +1,5 @@
+﻿$User = "pranesh.s@horizontal.com"
+$PWord = ConvertTo-SecureString -String "nBZwJ4MCLjS3RIsa" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+$body= "Test Mail"
+Send-MailMessage -To “pranesh.s@horizontal.com” -From “noreply@horizontal.com”  -Subject "Test Mail" -Body $body -BodyAsHtml -Credential $Credential -SmtpServer “smtp-relay.sendinblue.com” -Port 587
