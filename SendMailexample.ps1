cd2f6491aee046d3a09ee326b371fee1238005cb
praneshshzl <69966286+praneshshzl@users.noreply.github.com>
2020-11-02 20:59:41 +0530

    Add files via upload

---
+++ SendMailexample.ps1
@@ -0,0 +1,7 @@
+﻿$paramuname=$args[0]
+$parampwd=$args[1]
+$User = "$paramuname"
+$PWord = ConvertTo-SecureString -String "$parampwd" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+$body = "Hello world"
+Send-MailMessage -To “pranesh.s@horizontal.com” -From “noreply@horizontal.com”  -Subject "Hello world" -Body $body -BodyAsHtml -Credential $Credential -SmtpServer “smtp-relay.sendinblue.com” -Port 587
